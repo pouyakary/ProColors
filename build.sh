1bffe4357c8453463fd156c7c952be0f64223c98
pmkary <pouya@kary.us>
2018-05-04 00:14:20 +0430

    commented

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 
-themex pro-colors.themeX
+#
+# ─── BUILDING ───────────────────────────────────────────────────────────────────
+#
 
-cp ./pro-colors.themeX/builds/sublime/karyprocolors-dark.tmTheme  ~/.vscode/extensions/pro-colors/tmThemes
-cp ./pro-colors.themeX/builds/sublime/karyprocolors-light.tmTheme ~/.vscode/extensions/pro-colors/tmThemes
-cp ./pro-colors.themeX/builds/sublime/karyprocolors-minimaldark.tmTheme ~/.vscode/extensions/pro-colors/tmThemes
+    themex pro-colors.themeX
 
+#
+# ─── INSTALLING ON VISUAL STUDIO CODE ───────────────────────────────────────────
+#
+
+    cp ./pro-colors.themeX/builds/sublime/karyprocolors-dark.tmTheme  ~/.vscode/extensions/pro-colors/tmThemes
+    cp ./pro-colors.themeX/builds/sublime/karyprocolors-light.tmTheme ~/.vscode/extensions/pro-colors/tmThemes
+    cp ./pro-colors.themeX/builds/sublime/karyprocolors-minimaldark.tmTheme ~/.vscode/extensions/pro-colors/tmThemes
+
+# ────────────────────────────────────────────────────────────────────────────────
