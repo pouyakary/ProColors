c332684c92cf88823e24ec92abe66f47284baaa7
pmkary <pouya@kary.us>
2019-04-24 13:53:14 +0430

    Added the RegExp language !

--- build.sh
+++ build.sh
@@ -1,16 +1,36 @@
 
 #
+# ─── PATHS ──────────────────────────────────────────────────────────────────────
+#
+
+    BUILD_PATH="./pro-colors.themeX/builds/sublime"
+    VSCODE_PATH="~/.vscode/extensions/pro-colors/tmThemes"
+    SUBLIME_PATH='~/Library/"Application Support"/"Sublime Text 3"/Packages/User'
+
+#
 # ─── BUILDING ───────────────────────────────────────────────────────────────────
 #
 
     themex pro-colors.themeX
 
 #
+# ─── COPY TO DESTINATIONS ───────────────────────────────────────────────────────
+#
+
+    function copy_to_destination ( ) {
+        cp $BUILD_PATH/karyprocolors-dark.tmTheme $1/karyprocolors-dark.tmTheme
+        cp $BUILD_PATH/karyprocolors-light.tmTheme $1/karyprocolors-light.tmTheme
+        cp $BUILD_PATH/karyprocolors-minimaldark.tmTheme $1/karyprocolors-minimaldark.tmTheme
+    }
+
+#
 # ─── INSTALLING ON VISUAL STUDIO CODE ───────────────────────────────────────────
 #
 
-    cp ./pro-colors.themeX/builds/sublime/karyprocolors-dark.tmTheme  ~/.vscode/extensions/pro-colors/tmThemes
-    cp ./pro-colors.themeX/builds/sublime/karyprocolors-light.tmTheme ~/.vscode/extensions/pro-colors/tmThemes
-    cp ./pro-colors.themeX/builds/sublime/karyprocolors-minimaldark.tmTheme ~/.vscode/extensions/pro-colors/tmThemes
+    ## vscode
+    copy_to_destination $VSCODE_PATH
+
+    ## sublime text
+    copy_to_destination $SUBLIME_PATH
 
 # ────────────────────────────────────────────────────────────────────────────────
